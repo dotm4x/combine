5c5a960a0640f614d148445c805b92742feb5c94
Max <max@stonebogus.com>
2026-08-02 22:39:25 -0600

    refactor: update Composer class to accept settings and enhance composition handling

--- source/composers/composer.ts
+++ source/composers/composer.ts
@@ -1,4 +1,11 @@
-export class Composer {
-  public constructor() {
+export class Composer<ElementType = unknown> {
+  public readonly root: ElementType
+
+  public constructor(settings: { root: ElementType }) {
+    this.root = settings.root
+  }
+
+  public compose(): ElementType {
+    return this.root
   }
 }
